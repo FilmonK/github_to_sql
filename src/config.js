871d89b07e89da6aa6406c11c9ae171f61a76713
Filmon Kesete <filmonk@gmail.com>
2024-08-09 08:18:39 -0500

    config updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const GITHUB_OWNER = 'metabase';
-export const GITHUB_REPO = 'metabase';
+export const GITHUB_OWNER = '';
+export const GITHUB_REPO = '';
 export const GITHUB_QUERY = `repo:${GITHUB_OWNER}/${GITHUB_REPO} is:issue is:open label:"Type:New Feature"`;
 export const PER_PAGE = 10; 
 export const RATE_LIMIT_RESET_BUFFER = 60 * 1000;
